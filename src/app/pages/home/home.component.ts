a947a9150bff2828f488b82d846b89a955427128
JuanCruu <juancruznavarro592@gmail.com>
2024-08-03 18:53:05 -0300

    making basic template and adding axios library

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlbumsService } from '../../services/albums.service';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class HomeComponent {
+  albums: any
+  filterWorld = ''
 
-  constructor() { }
+  constructor(private albumsService: AlbumsService) { }
+  async ngOnInit() {
+    try {
+      const response = await this.albumsService.getPhotos();
+      this.albums = response
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+    }
+  }
+
+
+  getFilterPhotos() {
+    if (this.filterWorld === '') {
+      return this.albums
+    }
+    return this.albums.filter((album: any) => album.title.includes(this.filterWorld))
+  }
+
 }
---
+++ src/app/services/albums.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumsService {
+
+  constructor() {}
+
+  private apiUrl = 'https://jsonplaceholder.typicode.com/';
+
+
+  async getPhotos(page: number = 1) {
+
+    try {
+
+      let response = await axios.get(`${this.apiUrl}/albums/${page}/photos`, {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+
+      if (response.status === 200) {
+        return response.data
+      }
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+
+}
